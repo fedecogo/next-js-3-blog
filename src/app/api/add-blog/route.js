43608742f13b528c4a39bca62032899d717c06ab
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-08-06 12:02:52 +0200

    fix import

--- src/app/api/add-blog/route.js
+++ src/app/api/add-blog/route.js
@@ -1,5 +1,5 @@
-import connectToDB from "@/database";
-import Blog from "@/models/blog";
+import connectToDB from "../../database";
+import Blog from "../../models/blog";
 import Joi from "joi";
 import { NextResponse } from "next/server";
 
